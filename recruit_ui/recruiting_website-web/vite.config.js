6ccb7fad8b9c40f526796bfe238af9dfec32bcf9
fjj <fjj@qq.com>
2025-04-10 09:10:04 +0800

    fix(ui): 添加附件数量限制及注释调整

--- recruit_ui/recruiting_website-web/vite.config.js
+++ recruit_ui/recruiting_website-web/vite.config.js
@@ -7,26 +7,27 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
+  // server: {
+  //   proxy: {
       // /company
-      '^/api': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '^/admin': {
-        target: 'http://127.0.0.1:8080',
-      },
-      '^/user/': {
-        target: 'http://127.0.0.1:8080',
-      },
-    },
-  },
+      // '^/api': {
+      //   target: 'http://127.0.0.1:8080',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+      // '^/admin': {
+      //   target: 'http://127.0.0.1:8080',
+      // },
+      // '^/user/': {
+      //   target: 'http://127.0.0.1:8080',
+      // },
+  //   },
+  // },
 })
