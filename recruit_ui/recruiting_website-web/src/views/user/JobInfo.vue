99d30f59f2b6b2f7c747562a5188ea5ebde547a4
fjj <fjj@qq.com>
2025-04-24 13:42:38 +0800

    增加岗位详情顶部距离

--- recruit_ui/recruiting_website-web/src/views/user/JobInfo.vue
+++ recruit_ui/recruiting_website-web/src/views/user/JobInfo.vue
@@ -288,7 +288,7 @@ p {
     padding: 10px;
     border-radius: 10px;
     background: #fff;
-    margin: 65px 20px 0 20px;
+    margin: 85px 20px 0 20px;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
 }
 .job-content {
